1df44fc7fd961c77d2d3ea8e4ad7248f6ceef904
writecodewithme <102635042+writecodewithme@users.noreply.github.com>
2023-01-26 17:01:47 -0500

    l

--- index.js
+++ index.js
@@ -17,3 +17,39 @@
 //     $('.hamburger-icon i').addClass('icofont-navigation-menu');
 //     $('.hamburger-icon i').removeClass('icofont-close');
 // });
+
+const hamburgerIcon= document.getElementById("navigation-icon")
+const closeMenu=document.getElementById("close-menu")
+const navigationIcon=document.getElementsByClassName(".bi-list")
+const navItems=document.getElementById("nav-items")
+
+hamburgerIcon.addEventListener("click",function(){
+       navItems.style.transform= "translateX(0px)"
+       navItems.style.transition="0.3s ease"
+       hamburgerIcon.style.display="none"
+       closeMenu.style.display="block"
+       })
+
+       closeMenu.addEventListener('click',()=>{
+              navItems.style.transform= "translateX(500px)"
+              navItems.style.transition="0.3s ease"
+              hamburgerIcon.style.display="block"
+              closeMenu.style.display="none"
+
+       })
+
+      function dissappernav(){
+              navItems.style.display="none"
+       }
+
+       
+
+      
+      
+      
+
+
+
+
+   
+

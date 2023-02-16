c389aad91186629ae6ea6c8d95889fec171c6047
writecodewithme <102635042+writecodewithme@users.noreply.github.com>
2023-02-16 14:36:15 -0500

    s

--- index.js
+++ index.js
@@ -28,23 +28,28 @@ hamburgerIcon.addEventListener("click",function(){
        navItems.style.transition="0.3s ease"
        hamburgerIcon.style.display="none"
        closeMenu.style.display="block"
+       
        })
 
        closeMenu.addEventListener('click',()=>{
+              
               navItems.style.transform= "translateX(500px)"
               navItems.style.transition="0.3s ease"
               hamburgerIcon.style.display="block"
               closeMenu.style.display="none"
               window.scrollBy=("0,0")
+       
 
        })
        $('nav ul li a').on('click', function () {
-                 $('nav ul').css("transform", "translatex(500px)");
-              hamburgerIcon.style.display='block';
-              closeMenu.style.display='none'
+                 if(window.innerWidth<600){
+                     $('nav ul').css("transform", "translatex(500px)");
+                      hamburgerIcon.style.display='block';
+                     closeMenu.style.display='none'
+       }
                });
 
-
+      
      
 
       

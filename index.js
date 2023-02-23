6ef39e95850a2de99e4e1fb0381079cf29072899
writecodewithme <102635042+writecodewithme@users.noreply.github.com>
2023-02-23 16:35:21 -0500

    changes

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-// $('.slider').slick();
 
-// $('.hamburger-icon').on('click', function () {
-//     if ($('.hamburger-icon i').hasClass('icofont-navigation-menu')) {
-//         $('nav ul').css("display", "flex");
-//         $('.hamburger-icon i').removeClass('icofont-navigation-menu');
-//         $('.hamburger-icon i').addClass('icofont-close');
-//     } else {
-//         $('nav ul').css("display", "none");
-//         $('.hamburger-icon i').addClass('icofont-navigation-menu');
-//         $('.hamburger-icon i').removeClass('icofont-close');
-//     }
-// });
-
-// $('nav ul li a').on('click', function () {
-//     $('nav ul').css("display", "none");
-//     $('.hamburger-icon i').addClass('icofont-navigation-menu');
-//     $('.hamburger-icon i').removeClass('icofont-close');
-// });
 
 const hamburgerIcon= document.getElementById("navigation-icon")
 const closeMenu=document.getElementById("close-menu")
